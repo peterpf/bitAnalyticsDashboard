6a440f7fac26ef020c02461f7dd64a476403db45
Peter Pfaffeneder <peter.pfaffeneder@gmail.com>
2018-08-21 20:04:43 +0200

    Adapted mock server, correct parsing of request data

--- mock/server.js
+++ mock/server.js
@@ -1,128 +1,118 @@
 const express = require('express')
 const app = express()
 
-app.use((request, response, next) => {
-  console.log(request.headers)
+const APP_PORT = 3001;
+
+app.use((req, res, next) => {
+  console.log(req.url)
   next()
 })
 
-app.use((request, response, next) => {
-  request.chance = Math.random()
-  next()
+app.get('/test', (req, res) => {
+  const id = req.query.id;
+  res.json({ someTestField: 'Hello World! ' + id })
 })
 
-// Instances
-app.get('/query?query=count(up{job="analytics"} == 1) by (continent)', (request, response) => {
-  response.json({
-    "status": "success",
-    "data": {
-      "resultType": "vector",
-      "result": [
-        {
-          "metric": {
-            "continent": "europe"
+// reqs per minute
+app.get('/query', (req, res) => {
+  const query = req.query.query;
+  let result = {};
+  // Instances
+  if (query === 'count(up{job="analytics"} == 1) by (continent)') {
+    result = {
+      status: "success",
+      data: {
+        resultType: "vector",
+        result: [
+          {
+            metric: {
+              continent: "europe"
+            },
+            value: [
+              1534777117.45,
+              "2"
+            ]
           },
-          "value": [
-            1534777117.45,
-            "2"
-          ]
-        },
-        {
-          "metric": {
-            "continent": "us"
-          },
-          "value": [
-            1534777117.45,
-            "3"
-          ]
-        }
-      ]
+          {
+            metric: {
+              continent: "us"
+            },
+            value: [
+              1534777117.45,
+              "3"
+            ]
+          }
+        ]
+      }
     }
-  })
+    // CPU EU
+  } else if (query === 'avg(node_load1{name=~".*ingress.*", continent="europe"})') {
+    result = {
+      status: "success",
+      data: {
+        resultType: "vector",
+        result: [
+          {
+            metric: {},
+            value: [
+              1534777119.906,
+              "0.2"
+            ]
+          }
+        ]
+      }
+    };
+    // CPU US
+  } else if (query === 'avg(node_load1{name=~".*ingress.*", continent="us"})') {
+    result = {
+      status: "success",
+      data: {
+        resultType: "vector",
+        result: [
+          {
+            metric: {},
+            value: [
+              1534777135.458,
+              "0.45999999999999996"
+            ]
+          }
+        ]
+      }
+    };
+    // Requests by continent
+  } else if (query === 'sum(rate(requests_total[1m])) by (continent)') {
+    result = {
+      status: "success",
+      data: {
+        resultType: "vector",
+        result: [
+          {
+            metric: {
+              continent: "europe"
+            },
+            value: [
+              1534777133.752,
+              "147.51785714285714"
+            ]
+          },
+          {
+            metric: {
+              continent: "us"
+            },
+            value: [
+              1534777133.752,
+              "300.359693877551"
+            ]
+          }
+        ]
+      }
+    };
+  } else {
+    result = { youAreOnTheRightPath: true }
+  }
+  res.json(result)
 })
 
-// CPU EU
-app.get('/query?query=avg(node_load1{name=~".*ingress.*", continent="europe"})', (request, response) => {
-  response.json({
-    "status": "success",
-    "data": {
-      "resultType": "vector",
-      "result": [
-        {
-          "metric": {},
-          "value": [
-            1534777119.906,
-            "0.2"
-          ]
-        }
-      ]
-    }
-  })
+app.listen(APP_PORT, () => {
+  console.log('Listening on Port ' + APP_PORT);
 })
-// CPU US
-app.get('/query?query=avg(node_load1{name=~".*ingress.*", continent="us"})', (request, response) => {
-  response.json({
-    "status": "success",
-    "data": {
-      "resultType": "vector",
-      "result": [
-        {
-          "metric": {},
-          "value": [
-            1534777135.458,
-            "0.45999999999999996"
-          ]
-        }
-      ]
-    }
-  })
-})
-// Requests per minute
-app.get('/query?query=sum(rate(requests_total[1m]))', (request, response) => {
-  response.json({
-    "status": "success",
-    "data": {
-      "resultType": "vector",
-      "result": [
-        {
-          "metric": {},
-          "value": [
-            1534777131.995,
-            "448.36734693877554"
-          ]
-        }
-      ]
-    }
-  })
-})
-// Requests per minute by country
-app.get('/query=sum(rate(requests_total[1m])) by (continent)', (request, response) => {
-  response.json({
-    "status": "success",
-    "data": {
-      "resultType": "vector",
-      "result": [
-        {
-          "metric": {
-            "continent": "europe"
-          },
-          "value": [
-            1534777133.752,
-            "147.51785714285714"
-          ]
-        },
-        {
-          "metric": {
-            "continent": "us"
-          },
-          "value": [
-            1534777133.752,
-            "300.359693877551"
-          ]
-        }
-      ]
-    }
-  })
-})
-
-app.listen(3000)
